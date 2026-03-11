f1c1452f073e6c794cd3373cc4e862c2ff5e0a06
Miguel <miguel2006rc@gmail.com>
2026-03-11 17:26:26 +0100

    terminado

--- src/jrr_alquilervehiculos/JRR_AlquilerVehiculos.java
+++ src/jrr_alquilervehiculos/JRR_AlquilerVehiculos.java
@@ -4,15 +4,8 @@
  */
 package jrr_alquilervehiculos;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.Scanner;
 import utiles.ES;
 import utiles.Utilidades;
 
