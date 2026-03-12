23fa469dccbbd61bdb1ade82e90aadf3457a8427
Jesusrr06 <jrodrem422@g.educaand.es>
2026-03-12 10:32:46 +0100

    Terminado

--- src/jrr_alquilervehiculos/JRR_AlquilerVehiculos.java
+++ src/jrr_alquilervehiculos/JRR_AlquilerVehiculos.java
@@ -1,3 +1,4 @@
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
@@ -694,6 +695,7 @@ public class JRR_AlquilerVehiculos {
         Alquiler a;
         Cliente c;
         Vehiculo v;
+        nAlquileres=0;
         int i = 0;
         String fichero = ES.leerArchivo(RUTA_A);
         String[] linea = fichero.split("\n");
